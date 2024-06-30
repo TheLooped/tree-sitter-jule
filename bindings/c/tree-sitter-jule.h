92b55c57a6e7a4c5b71ebbfa4670fadc19dedf1d
TheLooped <95268961+TheLooped@users.noreply.github.com>
2024-06-30 14:08:45 -0500

    feat: Initialize project structure

---
+++ bindings/c/tree-sitter-jule.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_JULE_H_
+#define TREE_SITTER_JULE_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_jule(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_JULE_H_
---
+++ grammar.js
@@ -0,0 +1,7 @@
+module.exports = grammar({
+  name: 'jule',
+
+  rules: {
+    source_file: $ => 'Hello Jule'
+  }
+});
